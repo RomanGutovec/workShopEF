fca28cfff52ec3c641daf84ff75b5946f828b5b2
Roman Gutovec <roman.gutovec@gmail.com>
2019-02-28 21:51:50 +0300

    Create Item entity with appropriate mapping. Create InitialMigrat

---
+++ DAL/Context/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using DAL.Configurations;
+using DAL.Entities;
+using DAL.Migrations;
+
+namespace DAL.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext() : base("InternationWidgets")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<AppDbContext, Configuration>());
+        }
+
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //TODO: setup dbcontext configuration using EF Fluent API. DO NOT add property attributes for any files from "Entities" folder.
+            modelBuilder.Configurations.Add(new ItemConfig());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ DAL/Migrations/201902281843210_Initial.cs
@@ -0,0 +1,36 @@
+namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.tbl_items",
+                c => new
+                {
+                    cln_item_id = c.Int(nullable: false, identity: true),
+                    cln_item_description = c.String(),
+                    cln_item_price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                })
+                .PrimaryKey(t => t.cln_item_id);
+
+            this.Sql(@"insert into tbl_items(cln_item_description, cln_item_price) values
+
+                 ('56'' Blue Freen', 3.5),
+
+                 ('Spline End (Xtra Large)', 0.25),
+
+                 ('3'' Red Freen', 12.0)
+
+                ");
+        }
+
+        public override void Down()
+        {
+            DropTable("dbo.tbl_items");
+        }
+    }
+}
