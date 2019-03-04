689d8af6c6b591e9b139890ea511e188e078f2e4
Roman Gutovec <roman.gutovec@gmail.com>
2019-03-04 11:53:34 +0300

    Add Order entity, Order migration with appropriate data.

--- DAL/Context/AppDbContext.cs
+++ DAL/Context/AppDbContext.cs
@@ -18,6 +18,7 @@ namespace DAL.Context
         {
             //TODO: setup dbcontext configuration using EF Fluent API. DO NOT add property attributes for any files from "Entities" folder.
             modelBuilder.Configurations.Add(new ItemConfig());
+            modelBuilder.Configurations.Add(new OrderConfig());
 
             base.OnModelCreating(modelBuilder);
         }
